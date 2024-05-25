276641f6c053a52e9601be22dc610456985dba16
yassib1 <86356206+yibork@users.noreply.github.com>
2024-05-25 22:17:50 +0100

    Update

--- joblistings/admin.py
+++ joblistings/admin.py
@@ -1,2 +1,14 @@
 from django.contrib import admin
+from .models import JobPage, Industry, Category, Currency, City, Company, ContactPerson, ContactIntern, CoddingLanguages, Languages
+admin.site.register(JobPage)
+admin.site.register(Industry)
+admin.site.register(Category)
+admin.site.register(Currency)
+admin.site.register(City)
+admin.site.register(Company)
+admin.site.register(ContactPerson)
+admin.site.register(ContactIntern)
+admin.site.register(CoddingLanguages)
+admin.site.register(Languages)
+# Path: joblistings/models.py
 
